5c51933a60d14086735be2ca21861df304382762
islam1s <islam.saied.231@gmail.com>
2023-10-21 21:19:28 +0300

    java learning

---
+++ my_first_java_project/src/my_first_java_project_1/main.java
@@ -0,0 +1,131 @@
+/* 
+package my_first_java_project_1;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+*/		
+	/*
+	 
+	 	//first steps.
+	 
+		System.out.println("I LOVEEEEE pizza");
+		System.out.println("Because pizza is yum yum");
+		System.out.print("testing escape squence \n");
+		System.out.print("bla \t bla \t bla \n");
+		System.out.print("\"testing adding qoutes and a backslash \\ \"\n\n");
+
+		long x = 4206900000000000L;
+		double d = 3.145684512;
+		char c = 's';
+		String word = "ya bedany";
+		
+		System.out.println("my X    variable has a value of: " + x);
+		System.out.println("my C    variable has a value of: " + c);
+		System.out.println("my d    variable has a value of: " + d);
+		System.out.println("my word variable has a value of: " + word);
+		*/
+		
+//#####################################################################################
+		
+		/*
+		
+		//manipulating variables - switching two variables
+		
+		String x = "drink 1 water";
+		String y = "drink 2 berill";
+		String temp;
+		
+		temp = x;
+		x    = y;
+		y    = temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+		
+		*/
+		
+		
+//	} 
+
+//}
+
+//#####################################################################################
+
+	// accepting user inputs - scanner
+/*
+package my_first_java_project_1;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in); // this creates the scanner object
+		//      ^^^^^^^
+		//    this the name.   
+		//    it can take any name
+		
+		System.out.println("what is your name? ");
+		String name = scanner.nextLine();
+		System.out.println("whats your age? ");
+		int age =scanner.nextInt();
+		scanner.nextLine();
+	//  ^^^^^^^^^^^^^^^^^^^
+	//  this is to clear out the remaining "\n" from the "scan_Int" 	
+		System.out.println("what is your fav food? ");
+		String fav_food = scanner.nextLine();
+		
+		System.out.println("\nhello "+name);
+		System.out.println("your age is  "+age);
+		System.out.println("you like  "+fav_food);
+
+		
+		
+		
+	}
+}
+*/
+
+//#####################################################################################
+
+/*
+
+package my_first_java_project_1;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	//	int days_with_her = 246;
+	//	double days_with_her = 247;
+	//	days_with_her = days_with_her + 1;
+	//	days_with_her = days_with_her - 1;
+	//	days_with_her = days_with_her * 1;
+	//	days_with_her = days_with_her / 1;
+	//	days_with_her = days_with_her % 1;
+	//	days_with_her ++;
+	//	days_with_her = (double) days_with_her / 3
+				;
+		
+		System.out.println(days_with_her);
+		
+	}
+}
+*/
+
+//#####################################################################################
+
+package my_first_java_project_1;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(days_with_her);
+		
+	}
+}
